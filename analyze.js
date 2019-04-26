09b9b8ad9e57776161ac67240a41bbfd7f327af8
Maggie Wiseman <maggie.wiseman@gmail.com>
2019-04-26 10:06:30 -0400

    edited analyze

--- analyze.js
+++ analyze.js
@@ -4,29 +4,27 @@ var estraverse = require("estraverse");
 var util = require("util");
 var escodegen = require("escodegen");
 
-var declaredVariables = [];
-
 function analyzeCode(code) {
-    var ast = esprima.parse(code);
-    traverse(ast);
-    turnItBackIntoCode(ast);
+    // var ast = esprima.parse(code);
+    // traverse(ast);
+    // turnItBackIntoCode(ast);
 }
 
-function traverse(ast) {
-    estraverse.traverse(ast, {
-        enter: function(node) {
-            if (node.type == "Identifier") {
-                node.name = node.name + "_exploreLearning";
-            }
-        }
-    });
-    console.log(util.inspect(ast, false, null, true));
-}
+// function traverse(ast) {
+//     estraverse.traverse(ast, {
+//         enter: function(node) {
+//             if (node.type == "Identifier") {
+//                 node.name = node.name + "_exploreLearning";
+//             }
+//         }
+//     });
+//     console.log(util.inspect(ast, false, null, true));
+// }
 
-function turnItBackIntoCode(ast) {
-    var code = escodegen.generate(ast);
-    console.log(util.inspect(code, false, null, true));
-}
+// function turnItBackIntoCode(ast) {
+//     var code = escodegen.generate(ast);
+//     console.log(util.inspect(code, false, null, true));
+// }
 
 /* -------------------------------------------------------------------------------------------- */
 // var fs = require("fs");
@@ -48,65 +46,25 @@ function turnItBackIntoCode(ast) {
 //     });
 // }
 
-// // 2
-// if (process.argv.length < 3) {
-//     console.log("Usage: analyze.js file.js");
-//     process.exit(1);
-// }
-
-// // 3
-// var filename = process.argv[2];
-// console.log("Reading " + filename);
-// var code = fs.readFileSync(filename, "utf-8");
-
-// analyzeCode(code);
-// console.log("Done");
 /* ----------------------------------------------------------------------------- */
-// var fs = require("fs");
-// var esprima = require("esprima");
+var fs = require("fs");
+var esprima = require("esprima");
 
-// function analyzeCode(code) {
+function analyzeCode(code) {
+    // var ast = esprima.parse(code);
+    // console.log(ast);
+}
 
-// }
-
-// // 2
-// if (process.argv.length < 3) {
-//     console.log("Usage: analyze.js file.js");
-//     process.exit(1);
-// }
-
-// // 3
-// var filename = process.argv[2];
-// console.log("Reading " + filename);
-// var code = fs.readFileSync(filename, "utf-8");
-
-// analyzeCode(code);
-// console.log("Done");
 /* ----------------------------------------------------- */
 
-// var fs = require("fs");
-// var esprima = require("esprima");
+if (process.argv.length < 3) {
+    console.log("Usage: analyze.js file.js");
+    process.exit(1);
+}
 
-// function analyzeCode(code) {
-//     var ast = esprima.parse(code);
-//     console.log(ast);
-// }
-
-// // 2
-// if (process.argv.length < 3) {
-//     console.log("Usage: analyze.js file.js");
-//     process.exit(1);
-// }
-
-// // 3
 var filename = process.argv[2];
 console.log("Reading " + filename);
 var code = fs.readFileSync(filename, "utf-8");
 
 analyzeCode(code);
 console.log("Done");
-
-// function analyze(code) {
-//     var ast = esprima.parse(code);
-//     console.log(ast);
-// }
